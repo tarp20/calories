83fb14fdb380f071afa616005446c02cf2dc6888
Taras Piont <taras.piont@gmail.com>
2022-04-02 23:53:56 +0200

    fix

--- main.py
+++ main.py
@@ -50,4 +50,6 @@ app.add_url_rule(
 )
 
 
-# app.run(debug=True)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
